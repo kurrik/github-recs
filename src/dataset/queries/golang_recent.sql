9d8153e69e39881c461334a3044c8e6c37adc1f8
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-12-04 00:29:47 -0800

    Extend recent duration to 4 months for golang.

--- src/dataset/queries/golang_recent.sql
+++ src/dataset/queries/golang_recent.sql
@@ -2,7 +2,7 @@ SELECT type, repository_url, actor
   FROM [githubarchive:github.timeline]
   WHERE
     type="PushEvent"
-    AND PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC("2013-10-01 00:00:00")
+    AND PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC("2013-08-01 00:00:00")
     AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC("2013-12-01 00:00:00")
     AND repository_language="Go"
   GROUP EACH BY type, repository_url, actor
